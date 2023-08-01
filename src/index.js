26c584e3a057da2dbe93239e16c0b2f8cb16eb0e
ekat-velaz <ekatvelaz@gmail.com>
2023-08-01 17:02:44 +0100

    Create header and nav with styling

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import "./style.css";
+
+// make drop-down menu not to disappear when you click on it
+// (and also include toggle of active class if youll have many menu buttons)
+
+document.addEventListener("click", (e) => {
+  const isDropdownBtn = e.target.matches("[data-dropdown-btn]");
+  if (!isDropdownBtn && e.target.closest("[data-dropdown]") != null) return;
+
+  let currentDropdown;
+  if (isDropdownBtn) {
+    currentDropdown = e.target.closest("[data-dropdown]");
+    currentDropdown.classList.toggle("active");
+  }
+
+  document.querySelectorAll("[data-dropdown].active").forEach((dropdown) => {
+    if (dropdown === currentDropdown) return;
+    dropdown.classList.remove("active");
+  });
+});
